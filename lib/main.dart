9435d917c47f22da8770f0bbfaf28382f11c6681
naflanmohamed <naflanmohammed611@gmail.com>
2025-03-10 06:34:06 +0500

    Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,54 +7,80 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('My First Flutter App'),
-              backgroundColor: Colors.blue,
-              actions: const[
-                Padding(
-                  padding: EdgeInsets.only(right: 20),
-                  child: Icon(Icons.info)
-                )
-              ]
-            ),
-            drawer: Drawer(
-              child: ListView(
-                children: [
-                  ListTile(
-                    leading: const Icon(Icons.people),
-                    title: const Text('People'),
-                    onTap: (){},
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.train),
-                    title: const Text('Train'),
-                    onTap: (){},
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.bike_scooter),
-                    title: const Text('Bike'),
-                    onTap: (){},
-                  ),
-
-
-                ],
-              )
-            ),
-            body: Center(
-              child: Text('Hello, Flutter!',
-                style: TextStyle(fontSize: 24),
-              ),
-            )
-        )
+      home: MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String buttonText = 'Click here';
+
+  void changeText() {
+    setState(() {
+      buttonText = 'Clicked';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First Flutter App'),
+        backgroundColor: Colors.blue,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: Icon(Icons.info),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.people),
+              title: const Text('People'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.train),
+              title: const Text('Train'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.bike_scooter),
+              title: const Text('Bike'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hello, Flutter!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: changeText,
+              child: Text(buttonText),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
